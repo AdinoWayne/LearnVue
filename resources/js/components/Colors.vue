684dfa51dbfa53b2f1f55dfd8b6d672ad8675264
AdinoWayne <gnurt10@gmail.com>
2019-06-24 00:12:59 +0700

    lesson 3

--- resources/js/components/Colors.vue
+++ resources/js/components/Colors.vue
@@ -1,5 +1,5 @@
 <template>
-    <p>About</p>
+    <h1 class="text-3xl font-normal text-grey-darkest">Colors</h1>
 </template>
 
 <script>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,18 +1,55 @@
-import Home from './components/Home';
-import About from './components/About';
-
+import Logo from './components/Logo';
+import  LogoSymbol from './components/LogoSymbol';
+import Colors from './components/Colors';
+import Typography from './components/Typography';
+import Mascot from './components/Mascot';
+import Illustrations from './components/Illustrations';
+import LoadersAndAnimations from './components/LoadersAndAnimations';
+import Wallpapers from './components/Wallpapers';
 
 export default {
     mode: 'history',
+    linkActiveClass: 'font-bold',
     routes: [
         {
             path: '/',
-            component: Home
+            component: Logo,
+	        name: 'Logo'
         },
         {
-            path: '/about',
-            component: About,
-            name: 'about'
-        }
+            path: '/logo-symbol',
+            component: LogoSymbol,
+            name: 'LogoSymbol'
+        },
+        {
+            path: '/colors',
+            component: Colors,
+	        name: 'Colors'
+        },
+        {
+            path: '/typography',
+            component: Typography,
+	        name: 'Typography'
+        },
+        {
+            path: '/mascot',
+            component: Mascot,
+	        name: 'Mascot'
+        },
+        {
+            path: '/illustrations',
+            component: Illustrations,
+	        name: 'Illustrations'
+        },
+        {
+            path: '/loaders-and-animations',
+            component: LoadersAndAnimations,
+	        name: 'LoadersAndAnimations'
+        },
+        {
+            path: '/wallpapers',
+            component: Wallpapers,
+	        name: 'Wallpapers'
+        },
     ]
 }
